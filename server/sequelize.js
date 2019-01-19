8eb126018eff29d9485743e6317f57f4d098daee
Derrick White <gr8white@uga.edu>
2019-01-19 10:43:44 -0600

    No changes

--- server/sequelize.js
+++ server/sequelize.js
@@ -5,25 +5,25 @@ const Sequelize = require('sequelize')
 const db = require('./models')
 
 db.userdata.findAll()
-    .then((results) => {
-        results.forEach(function(index){
-            console.log(index.userid, index.f_name)
-        })
-    })
-
-models.post.findByID(10).then(function(post){
-    console.log(post)
-})
-
-db.useraccounts.create({email:'testemail@email.com',password:'this-is-a-password-hash'})
-.then(function(user){
-    console.log(user)
-})
-
-db.userdata.findAll({where: {l_name: 'Ashard'}})
   .then((results) => {
-    results.forEach(function(index){
-            console.log(index.userid, index.f_name, index.l_name);
-        })
-  });
+    results.forEach(function (index) {
+      console.log(index.userid, index.f_name)
+    })
+  })
 
+models.post.findByID(10).then(function (post) {
+  console.log(post)
+})
+
+db.useraccounts.create({ email: 'testemail@email.com', password: 'this-is-a-password-hash' })
+// 'useraccounts' (above) is the name of the table
+  .then(function (user) {
+    console.log(user)
+  })
+
+db.userdata.findAll({ where: { l_name: 'Ashard' } })
+  .then((results) => {
+    results.forEach(function (index) {
+      console.log(index.userid, index.f_name, index.l_name)
+    })
+  })
